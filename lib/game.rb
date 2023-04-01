b623c94c3ff2274d8ad1fed879713b9ee175f2b1
nuriakgd@gmail.com <nuria>
2023-04-01 17:10:51 +0200

    initial commit

---
+++ lib/game.rb
@@ -0,0 +1,66 @@
+class Game
+  TOTAL_ERRORS_ALLOWED = 7
+
+  def self.normalize(letter)
+    case letter
+    when "Ё"
+      "Е"
+    when "Й"
+      "И"
+    else
+      letter
+    end
+  end
+
+  def initialize(word)
+    @letters = word.upcase.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def letters_to_guess
+    @letters.map do |letter|
+      letter if @user_guesses.include?(self.class.normalize(letter))
+    end
+  end
+
+  def lost?
+    errors_allowed == 0
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    normalized_letter = self.class.normalize(letter)
+    if !over? && !@user_guesses.include?(normalized_letters)
+      @user_guesses << normalized_letter
+    end
+  end
+
+  def won?
+    (normalized_letters - @user_guesses).empty?
+  end
+
+  def word
+    @letters.join
+  end
+
+  private
+
+  def normalized_letters
+    @letters.map { |letter| self.class.normalize(letter) }
+  end
+end
